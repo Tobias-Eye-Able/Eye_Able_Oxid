36d3e23e5a66db48183d0e737024116daec73bc6
Tobias Greiner <139900571+Tobias-Eye-Able@users.noreply.github.com>
2023-08-28 14:44:33 +0200

    Add files via upload

---
+++ metadata.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Copyright © Webinclusion GmbH. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => 'EyeAble_EyeAbleAssist',
+    'title'       => 'Eye-Able® Assist',
+    'description' =>  'This is the Eye-Able® Assist Module. It helps to improve the accesibility of your shop or website. Eye-Able® Assist allows customers to customize the website to their individual needs.',
+    'thumbnail'   => 'pictures/eye-able_logo_schwarz.png',
+    'version'     => '2.0.0',
+    'author'      => 'Webinclusion GmbH',
+    'url'         => 'https://eye-able.com/',
+    'email'       => 'info@eye-able.com',
+    'extend'      => [],
+];
